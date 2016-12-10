371d80de0829785fa6be472a27ddd95a80bd1923
squirr3lking <sharo_shiri999@yahoo.com>
2016-12-10 00:11:12 -0500

    5

--- mod5_solution/js/script.js
+++ mod5_solution/js/script.js
@@ -101,7 +101,9 @@ function buildAndShowHomeHTML (categories) {
       // TODO: STEP 2: Here, call chooseRandomCategory, passing it retrieved 'categories'
       // Pay attention to what type of data that function returns vs what the chosenCategoryShortName
       // variable's name implies it expects.
+      chooseRandomCategory (categories) {
        var chosenCategoryShortName = categories.randomArrayIndex;
+      };
 
 
       // TODO: STEP 3: Substitute {{randomCategoryShortName}} in the home html snippet with the
