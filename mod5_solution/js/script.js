619c09861fa21f3553c897e6da388012f32d82b8
squirr3lking <sharo_shiri999@yahoo.com>
2016-12-10 00:20:32 -0500

    4

--- mod5_solution/js/script.js
+++ mod5_solution/js/script.js
@@ -40,8 +40,7 @@ var showLoading = function (selector) {
 // with propValue in given 'string'
 var insertProperty = function (string, propName, propValue) {
   var propToReplace = "{{" + propName + "}}";
-  string = string
-    .replace(new RegExp(propToReplace, "g"), propValue);
+  string = string.replace(new RegExp(propToReplace, "g"), propValue);
   return string;
 };
 
