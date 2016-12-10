27177772e185b4d3932e4d52f2ed2b7a83d5a826
squirr3lking <sharo_shiri999@yahoo.com>
2016-12-10 00:56:46 -0500

    Solution 5

--- mod5_solution/js/script.js
+++ mod5_solution/js/script.js
@@ -40,7 +40,8 @@ var showLoading = function (selector) {
 // with propValue in given 'string'
 var insertProperty = function (string, propName, propValue) {
   var propToReplace = "{{" + propName + "}}";
-  string = string.replace(new RegExp(propToReplace, "g"), propValue);
+  string = string
+   .replace(new RegExp(propToReplace, "g"), propValue);
   return string;
 };
 
